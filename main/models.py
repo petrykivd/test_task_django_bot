730a0f94019877b2c727f2cc02562ee3f0816ce5
petrykivd <petrykiv.dmytro19@gmail.com>
2024-02-16 19:27:32 +0200

    Created message model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
@@ -7,5 +6,24 @@ class User(models.Model):
     tg_user_id = models.IntegerField(unique=True, null=False)
     username = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
-    chat_id = models.IntegerField(null=False, unique=True)
     date_joined = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"#{self.tg_user_id} {self.username}"
+
+    class Meta:
+        verbose_name = "Telegram User"
+        verbose_name_plural = "Telegram Users"
+
+
+class Message(models.Model):
+    user = models.ManyToManyField(User, related_name="messages")
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'Message to {self.user} at {self.created_at}'
+
+    class Meta:
+        verbose_name = "Message"
+        verbose_name_plural = "Messages"
