a2083841db1d3ee77d52b9f3b56ec7ce03b66dda
petrykivd <petrykiv.dmytro19@gmail.com>
2024-02-16 12:34:11 +0200

    Register user in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.forms import ProfileForm
+from main.models import User
+
+
+@admin.register(User)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "tg_user_id",
+        "username",
+        "first_name",
+        "chat_id",
+        "date_joined",
+    )
+    form = ProfileForm
